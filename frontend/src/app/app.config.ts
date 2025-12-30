3d71ae6f4f7f470137f9cfa3ab50e73efa61fc23
Vyshnavi21504 <Vyshnavi21504@gmail.com>
2025-12-30 22:11:49 +0530

    done changes

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http'; 
-// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +10,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withFetch()), 
     provideAnimations(),
-    // provideAnimationsAsync()
   ]
 };
