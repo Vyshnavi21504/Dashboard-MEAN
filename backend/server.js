c3632b7dfb39b13262f360e8a48e394e86a488ac
Vyshnavi21504 <Vyshnavi21504@gmail.com>
2025-12-30 23:18:51 +0530

    fix done

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,15 @@ const app = express();
 
 app.use(express.json());
  
-// app.use(cors({
-//   origin: [
-//     'http://localhost:4200', 
-//     'https://dashboard-mean-frontend.onrender.com' 
-//   ],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
+app.use(cors({
+  origin: [
+    'http://localhost:4200', 
+    'https://dashboard-frontend-6tkm.onrender.com' 
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected Successfully"))
