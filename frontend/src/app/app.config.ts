97da7e013ab0ad60ac34490aa06534b5bf6c55a3
Vyshnavi21504 <Vyshnavi21504@gmail.com>
2025-12-30 22:03:27 +0530

    fix

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http'; // 1. Import withFetch
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http'; 
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch()), 
-    provideAnimationsAsync()
+    provideAnimations(),
+    // provideAnimationsAsync()
   ]
 };
